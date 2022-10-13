0943612027cb157f22eb019ed4645777bec3d132
daniilSimakhin <danil.110@yandex.ru>
2022-10-13 23:39:15 +0700

    Init

---
+++ Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Даниил Симахин on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
