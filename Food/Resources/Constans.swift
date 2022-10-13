6571518c18c06cf157181482d88d90a3622c01a3
daniilSimakhin <danil.110@yandex.ru>
2022-10-14 00:03:32 +0700

    Add/Constans-and-image-menu

---
+++ Food/Resources/Constans.swift
@@ -0,0 +1,17 @@
+//
+//  Constans.swift
+//  Food
+//
+//  Created by Даниил Симахин on 14.10.2022.
+//
+
+import Foundation
+
+typealias C = Constans
+enum Constans {
+    enum Images {
+        enum TabBar {
+            static let menu = "Menu"
+        }
+    }
+}
---
+++ Food/Supporting/Extensions/Extension+TabBar.swift
@@ -0,0 +1,8 @@
+//
+//  Extension+TabBar.swift
+//  Food
+//
+//  Created by Даниил Симахин on 13.10.2022.
+//
+
+import Foundation
