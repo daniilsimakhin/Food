cf567620b1122c1b845596149300967b8ec66f77
daniilSimakhin <danil.110@yandex.ru>
2022-10-14 00:09:50 +0700

    Add/Extension-tabbar

--- Food/Supporting/Extensions/Extension+TabBar.swift
+++ Food/Supporting/Extensions/Extension+TabBar.swift
@@ -5,4 +5,17 @@
 //  Created by Даниил Симахин on 13.10.2022.
 //
 
-import Foundation
+import UIKit
+
+extension UITabBarController {
+    
+    func createNavigationController(_ viewController: UIViewController, _ title: String, _ imageName: String) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: viewController)
+        let tabBarItem = UITabBarItem()
+        tabBarItem.image = UIImage(named: imageName)
+        tabBarItem.title = title
+        navigationController.tabBarItem = tabBarItem
+        navigationController.navigationBar.prefersLargeTitles = false
+        return navigationController
+    }
+}
