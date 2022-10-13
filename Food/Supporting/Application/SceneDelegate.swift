7e1dba8e9801d36787eea2820edd82edc6d34345
daniilSimakhin <danil.110@yandex.ru>
2022-10-13 23:52:24 +0700

    Add/Implementetion-no-code

---
+++ Food/Supporting/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Food
+//
+//  Created by Даниил Симахин on 13.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let vc = ViewController()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
